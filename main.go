912daa1a79af2e1408ec626908ac82d37f25c007
Shaon Talukdar <stalukdar@aviatrix.com>
2023-01-21 14:02:25 -0800

    object description

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
+type Job struct {
+	Company           string `json:"companyName"`
+	JobRole           string `json:"jobRole"`
+	Location          string `json:"location"`
+	ApplicationPeriod bool   `json:"open"`
+}
+
 func main() {
 
 }
