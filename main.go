b44648d52c3d0fe5a8e494d06075c7e16ae70f60
fensterp <fensterp@students.cs.ubc.ca>
2023-01-21 15:17:40 -0800

    petr initial structure done

---
+++ main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+type Job struct {
+	Company  string `json: "company"`
+	Position string `json: "position"`
+	Location string `json: "location"`
+	Salary   int    `json: "salary"`
+	Link     string `json: "link"`
+	Visa     string `json: "visa"`
+	Open     bool   `json: "open"`
+}
+
+type UserData struct {
+	Password string `json:"password"`
+	Applied  []int  `json:"applied"`
+}
+
+type Users map[string]UserData
+
+func fetch(w http.ResponseWriter, req *http.Request) {
+
+	jsonFile, err := os.Open("jobs.json")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer jsonFile.Close()
+
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+
+	w.Write(byteValue)
+
+}
+
+func addUser(w http.ResponseWriter, req *http.Request) {
+
+}
+
+func login(w http.ResponseWriter, req *http.Request) {
+
+}
+
+func apply(w http.ResponseWriter, req *http.Request) {
+
+}
+
+func generateResume(w http.ResponseWriter, req *http.Request) {
+
+}
+
+func main() {
+	http.HandleFunc("/fetch", fetch)
+	http.HandleFunc("/adduser", addUser)
+	http.HandleFunc("/login", login)
+	http.HandleFunc("/apply", apply)
+	http.HandleFunc("/generateresume", generateResume)
+}
