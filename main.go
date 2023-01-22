798469826f9d8b76ce79f9cde60e99fd9bb46f42
Shaon Talukdar <stalukdar@aviatrix.com>
2023-01-21 22:44:16 -0800

    reset + sync

---
+++ main.go
@@ -0,0 +1,348 @@
+package main
+
+import (
+	"encoding/csv"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	s "nwHacks-backend/scraper"
+	"os"
+	"strconv"
+)
+
+type Job struct {
+	Company  string      `json: "company"`
+	Position string      `json: "position"`
+	Location string      `json: "location"`
+	Salary   int         `json: "salary"`
+	Link     string      `json: "link"`
+	Visa     bool        `json: "visa"`
+	Open     bool        `json: "open"`
+	LeetCode [][2]string `json: "leetcode"`
+}
+
+type UserData struct {
+	Name     string `json:"name"`
+	Password string `json:"password"`
+	Applied  []int  `json:"applied"`
+	Saved    []int  `json:"saved"`
+}
+
+type Jobs map[string]Job
+
+type Users map[string]UserData
+
+func fetch(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+
+	jsonFile, err := os.Open("resources/jobs.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer jsonFile.Close()
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+	w.Write(byteValue)
+}
+
+func addUser(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+
+	userName := req.FormValue("username")
+	password := req.FormValue("password")
+	name := req.FormValue("name")
+
+	jsonFile, err := os.Open("resources/users.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer jsonFile.Close()
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+
+	var users Users
+
+	json.Unmarshal(byteValue, &users)
+
+	userData := UserData{}
+	userData.Name = name
+	userData.Password = password
+	users[userName] = userData
+
+	file, _ := json.MarshalIndent(users, "", "  ")
+	_ = ioutil.WriteFile("resources/users.json", file, 0644)
+
+	w.Write([]byte("User " + userName + " has been succesfully added"))
+}
+
+func login(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+
+	userName := req.FormValue("username")
+	password := req.FormValue("password")
+
+	jsonFile, err := os.Open("resources/users.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer jsonFile.Close()
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+	var users Users
+	json.Unmarshal(byteValue, &users)
+
+	if users[userName].Password == password {
+		w.Write([]byte("Login Successful"))
+	} else {
+		w.Write([]byte("Login Failed"))
+	}
+}
+
+func apply(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+
+	userName := req.FormValue("username")
+	jobId, _ := strconv.Atoi(req.FormValue("jobid"))
+
+	jsonFile, err := os.Open("resources/users.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer jsonFile.Close()
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+	var users Users
+	json.Unmarshal(byteValue, &users)
+
+	user := users[userName]
+	user.Applied = append(users[userName].Applied, jobId)
+	users[userName] = user
+
+	file, _ := json.MarshalIndent(users, "", "  ")
+	_ = ioutil.WriteFile("resources/users.json", file, 0644)
+
+	w.Write([]byte("Successfuly marked the job as applied"))
+}
+
+func getApplied(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+
+	userName := req.FormValue("username")
+
+	jsonFile, err := os.Open("resources/users.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer jsonFile.Close()
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+	var users Users
+	json.Unmarshal(byteValue, &users)
+
+	jsonFile, err = os.Open("resources/jobs.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer jsonFile.Close()
+	byteValue, _ = ioutil.ReadAll(jsonFile)
+	var jobs Jobs
+	json.Unmarshal(byteValue, &jobs)
+
+	appliedIds := users[userName].Applied
+	appliedJobs := []Job{}
+
+	for _, id := range appliedIds {
+		appliedJobs = append(appliedJobs, jobs[strconv.FormatInt(int64(id), 10)])
+	}
+
+	file, _ := json.MarshalIndent(appliedJobs, "", "  ")
+	w.Write(file)
+}
+
+func getSaved(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+
+	userName := req.FormValue("username")
+
+	jsonFile, err := os.Open("resources/users.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer jsonFile.Close()
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+	var users Users
+	json.Unmarshal(byteValue, &users)
+
+	jsonFile, err = os.Open("resources/jobs.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer jsonFile.Close()
+	byteValue, _ = ioutil.ReadAll(jsonFile)
+	var jobs Jobs
+	json.Unmarshal(byteValue, &jobs)
+
+	savedIds := users[userName].Saved
+	savedJobs := []Job{}
+
+	for _, id := range savedIds {
+		savedJobs = append(savedJobs, jobs[strconv.FormatInt(int64(id), 10)])
+	}
+
+	file, _ := json.MarshalIndent(savedJobs, "", "  ")
+	w.Write(file)
+}
+
+func saveJob(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+
+	userName := req.FormValue("username")
+	jobId, _ := strconv.Atoi(req.FormValue("jobid"))
+
+	jsonFile, err := os.Open("resources/users.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer jsonFile.Close()
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+	var users Users
+	json.Unmarshal(byteValue, &users)
+
+	user := users[userName]
+	user.Saved = append(users[userName].Saved, jobId)
+	users[userName] = user
+
+	file, _ := json.MarshalIndent(users, "", "  ")
+	_ = ioutil.WriteFile("resources/users.json", file, 0644)
+
+	w.Write([]byte("Successfuly marked the job as saved"))
+}
+
+func uploadResume(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+
+	req.ParseMultipartForm(32 << 20)
+	file, handler, err := req.FormFile("uploadfile")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+
+	f, err := os.OpenFile("./test/"+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer f.Close()
+
+	byteValue, _ := ioutil.ReadAll(f)
+
+	fmt.Println(byteValue)
+}
+
+func generateResume(w http.ResponseWriter, req *http.Request) {
+
+}
+
+func homePage(w http.ResponseWriter, req *http.Request) {
+	w.Write([]byte("welcome!"))
+}
+
+func (jobs Jobs) parseGithub(github []s.Job) []string {
+
+	companies := []string{}
+
+	for i, s := range github {
+		companies = append(companies, s.Company)
+		key := strconv.FormatInt(int64(i), 10)
+		job := Job{}
+
+		job.Company = s.Company
+		job.Position = s.JobRole
+		job.Visa = true
+
+		if s.Status == "Closed" {
+			job.Open = false
+		} else {
+			job.Open = true
+		}
+
+		job.Link = s.Link
+		job.Location = s.Location
+
+		jobs[key] = job
+	}
+
+	return companies
+}
+
+func (jobs Jobs) getLeetcode() {
+	for key, element := range jobs {
+		csvFile, err := os.Open("resources/leetcode/" + element.Company + ".csv")
+
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer csvFile.Close()
+
+		fileReader := csv.NewReader(csvFile)
+		records, err := fileReader.ReadAll()
+
+		leetCode := [][2]string{}
+
+		if len(records) > 0 {
+			for i := 1; i < len(records); i++ {
+				entry := [2]string{records[i][0], records[i][1]}
+				leetCode = append(leetCode, entry)
+			}
+		}
+
+		element.LeetCode = leetCode
+
+		jobs[key] = element
+	}
+}
+
+func setUp() {
+
+	jobs := Jobs{}
+	jobData := s.ScrapeGithub()
+	companies := jobs.parseGithub(jobData)
+	fmt.Println(len(companies))
+
+	jobs.getLeetcode()
+	file, _ := json.MarshalIndent(jobs, "", "  ")
+	_ = ioutil.WriteFile("resources/jobs.json", file, 0644)
+}
+
+func main() {
+	setUp()
+
+	http.HandleFunc("/fetch", fetch)
+	http.HandleFunc("/adduser", addUser)
+	http.HandleFunc("/login", login)
+	http.HandleFunc("/apply", apply)
+	http.HandleFunc("/generateresume", generateResume)
+	http.HandleFunc("/homepage", homePage)
+	http.HandleFunc("/getsaved", getSaved)
+	http.HandleFunc("/getapplied", getApplied)
+	http.HandleFunc("/savejob", saveJob)
+	http.HandleFunc("/uploadresume", uploadResume)
+
+	fmt.Println("Up and running")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
---
+++ scraper/scraper.go
@@ -0,0 +1,127 @@
+package scraper
+
+import (
+	"fmt"
+	"net"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/gocolly/colly"
+)
+
+type GithubJobStructure struct {
+	Company  string `json:"companyName"`
+	JobRole  string `json:"jobRole"`
+	Location string `json:"location"`
+	Link     string `json:"link"`
+	Status   string `json:"status"`
+}
+
+type LevelsJobStructure struct {
+	Company string `json:"companyName"`
+	Pay     string `json:"pay"`
+}
+
+func ScrapeGithub() []GithubJobStructure {
+
+	c := colly.NewCollector()
+
+	c.WithTransport(&http.Transport{
+		DialContext: (&net.Dialer{
+			Timeout:   90 * time.Second,
+			KeepAlive: 60 * time.Second,
+			DualStack: true,
+		}).DialContext,
+		MaxIdleConns:          100,
+		IdleConnTimeout:       90 * time.Second,
+		TLSHandshakeTimeout:   10 * time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
+	})
+
+	var jobData []GithubJobStructure
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Scraping:", r.URL)
+	})
+
+	c.OnResponse(func(r *colly.Response) {
+		fmt.Println("Status:", r.StatusCode)
+	})
+	c.OnHTML("table > tbody", func(h *colly.HTMLElement) {
+		h.ForEach("tr", func(_ int, el *colly.HTMLElement) {
+			var link string
+			el.ForEach("a", func(_ int, el *colly.HTMLElement) {
+				link = el.Attr("href")
+			})
+			tableData := GithubJobStructure{
+				Company:  el.ChildText("td:nth-child(1)"),
+				Location: el.ChildText("td:nth-child(2)"),
+				Status:   el.ChildText("td:nth-child(3)"),
+				JobRole:  el.ChildText("td:nth-child(4)"),
+				Link:     link,
+			}
+			jobData = append(jobData, tableData)
+		})
+	})
+
+	c.OnError(func(r *colly.Response, err error) {
+		fmt.Println("Request URL:", r.Request.URL, "failed with response:", r, "\nError:", err)
+	})
+
+	_ = c.Visit("https://github.com/bsovs/Fall2023-Internships/blob/main/README.md")
+
+	return jobData
+}
+
+func ScrapeLevels(companies []string) []LevelsJobStructure {
+
+	var levelsData []LevelsJobStructure
+	// Initialize a new collector
+	c := colly.NewCollector()
+
+	c.WithTransport(&http.Transport{
+		DialContext: (&net.Dialer{
+			Timeout:   90 * time.Second,
+			KeepAlive: 60 * time.Second,
+			DualStack: true,
+		}).DialContext,
+		MaxIdleConns:          100,
+		IdleConnTimeout:       90 * time.Second,
+		TLSHandshakeTimeout:   10 * time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
+	})
+
+	// Set the callback function
+	c.OnHTML("title", func(e *colly.HTMLElement) {
+		completeText := e.Text
+
+		splitText := strings.Split(completeText, " ")
+
+		companyName := splitText[0]
+
+		length := len(splitText) - 5
+
+		//Amazon Software Engineer Intern Salaries | $69.70 / hr | Levels.fyi
+		pay := splitText[length]
+		tempJob := LevelsJobStructure{
+			Company: companyName,
+			Pay:     pay,
+		}
+		levelsData = append(levelsData, tempJob)
+
+	})
+
+	for index := range companies {
+		companyNameHyphenated := companies[index]
+		companyNameHyphenated = strings.Replace(companyNameHyphenated, " ", "_", -1)
+		_ = c.Visit("https://www.levels.fyi/internships/" + companyNameHyphenated + "/Software-Engineer-Intern/")
+
+	}
+
+	c.OnError(func(r *colly.Response, err error) {
+		fmt.Println("Request URL:", r.Request.URL, "failed with response:", r, "\nError:", err)
+	})
+
+	return levelsData
+}
